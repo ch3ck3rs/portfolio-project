ec81e99123921ca0c713df3e4ff79f00319a48d3
rabbit <lvcoffman@gmial.com>
2020-01-09 21:42:50 -0600

    implemented postgreSQL, created Job model

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# adding this will allow the superuser online to see this and make edits.....
+
+from .models import Job
+
+admin.site.register(Job)
